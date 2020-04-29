450b263636c214d1f8bfe597f0684f423a9e5c56
Segura <segura-deluna@mail.ru>
2020-04-29 18:58:40 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const selectAllElements = document.querySelectorAll('[data-select]');
+
+
+selectAllElements.forEach(function (item) {
+	item.addEventListener('click', function () {
+		const realSelect = this.nextElementSibling;
+
+		if (event.target.hasAttribute('data-item')) {
+			const itemTitle = event.target.getAttribute('data-item');
+			this.querySelector('[data-title]').textContent = event.target.getAttribute('data-item');
+			this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+
+			if (realSelect) {
+				realSelect.value = itemTitle;
+			}
+
+		} else {
+			this.querySelector('.header-select__dropdown').classList.toggle('hidden');
+		}
+	})
+});
+
+
+
+// * Carousel =======
+$(document).ready(function () {
+	$("#carousel-header").owlCarousel({
+		items: 1,
+		nav: true,
+		navText: [],
+		loop: true
+	});
+});
